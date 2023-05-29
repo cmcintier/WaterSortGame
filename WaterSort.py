62535f3e5636791ae26815707239e67a8f973a09
Chaney McIntier <cmcintier01@gmail.com>
2023-05-28 23:49:19 -0500

    added window title,  changed how the quit event is handled, added a test surface and displayed it to screen

--- WaterSort.py
+++ WaterSort.py
@@ -1,21 +1,28 @@
 import pygame
+from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((1280,720))
+pygame.display.set_caption("Water Sort")
 clock = pygame.time.Clock()
 running = True
+
+test_surface = pygame.Surface((100,200))
+test_surface.fill("red")
 
 while running:
 
     #Checks pygame event system to see if player has quit the game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            pygame.quit()
+            exit()
 
     screen.fill("grey")
 
-    pygame.display.flip()
+    screen.blit(test_surface, (200,100))
 
+    pygame.display.update()
     clock.tick(60)
 
 pygame.quit()
