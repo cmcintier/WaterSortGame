0941071d0ca9666ade7b347846496dccbfb088e7
Chaney McIntier <cmcintier01@gmail.com>
2023-05-30 06:50:56 -0500

    added user input and button to generate a puzzle of given size

--- WaterSort.py
+++ WaterSort.py
@@ -13,6 +13,34 @@ class Color(pygame.sprite.Sprite):
         pygame.draw.rect(self.image, color, pygame.Rect(parentTube.rect.x+25, parentTube.rect.y + (190/4*fillLevel), 50, 190/4))
         self.rect = self.image.get_rect(bottomleft = (parentTube.rect.x+25, parentTube.rect.y + (190/4*fillLevel)))
     
+class Button():
+    def __init__(self, x, y, width, height, buttonText = "Button", onclickFunction=None):
+        self.onclickFunction = onclickFunction
+
+        self.image = pygame.Surface([50, 190/4])
+        self.image.fill("white")
+        pygame.draw.rect(self.image, color, pygame.Rect(x, y, width, height))
+        self.rect = self.image.get_rect(bottomleft = (x,y))
+
+        self.buttonSurface = pygame.Surface((self.rect.width, self.rect.height))
+        self.buttonRect = pygame.Rect(self.rect.x, self.rect.y, self.rect.width, self.rect.height)
+
+        self.buttonSurf = test_font.render(buttonText, True, (20, 20, 20))
+    
+    def update(self):
+        self.buttonSurface.blit(self.buttonSurf, [
+            self.buttonRect.width/2 - self.buttonSurf.get_rect().width/2,
+            self.buttonRect.height/2 - self.buttonSurf.get_rect().height/2
+        ])
+        screen.blit(self.buttonSurface, self.buttonRect)
+
+    def onclick(self, puzzleSize):
+        tempColors = COLOR_LIST.copy()
+        colorList = []
+        for i in range(puzzleSize):
+            colorIndex = r.randint(0, len(tempColors) - 1)
+            colorList.append(tempColors.pop(colorIndex))
+        return self.onclickFunction(colorList)
 
 class Tube(pygame.sprite.Sprite):
     def __init__(self, x, y, colorList = []):
@@ -43,8 +71,6 @@ class Tube(pygame.sprite.Sprite):
     def removeColor(self):
         self.colorGroup.empty()
         return self.colors.pop(-1)
-
-                    
                     
 def checkCompatible(tube1, tube2):
     tube1FillHeight = len(tube1.colors)
@@ -86,10 +112,8 @@ def generatePuzzle(colorList):
             tube = Tube(screenWidth / numTubes * x + (screenWidth/4/ numTubes), 300, tubeColors)
             tubes.add(tube)
         else:
-            print("balls")
             tube = Tube(screenWidth / numTubes * x + (screenWidth/4/ numTubes), 300, [])
-            tubes.add(tube)
-    
+            tubes.add(tube)   
     return tubes
 
 def checkWinState(tubes):
@@ -111,14 +135,28 @@ clock = pygame.time.Clock()
 test_font = pygame.font.Font(None, 50)
 
 tubes = generatePuzzle(COLOR_LIST[0:6])
+
+# TEXT
 text_surface = test_font.render("Water Sort Puzzle", False, "Black")
 text_win = test_font.render("YOU WIN", False, "Green")
+
+
+user_text = ''
+input_rect = pygame.Rect(screenWidth / 2 - 50, 600, 100, 32)
+color_active = pygame.Color("lightskyblue3")
+color_passive = pygame.Color('chartreuse4')
+color = color_passive
+
+generatePuzzleButton = Button(screenWidth/2-25, 700, 25, 25, "Generate New Puzzle", generatePuzzle)
+active = False
 
 selectedTube = None
 
 while True:
+    newPuzzle = False
 
     tubes.update()
+    
 
     #Handling pygame's event system
     events =  pygame.event.get()
@@ -129,7 +167,8 @@ while True:
             exit()
         # MOUSEBUTTONUP Event to check collisions
         elif event.type == pygame.MOUSEBUTTONUP:
-            print(selectedTube)
+            if generatePuzzleButton.rect.collidepoint(event.pos):
+                tubes = generatePuzzleButton.onclick(int(user_text))
             for tube in tubes:
                 if tube.rect.collidepoint(event.pos):
                     print(tube.colors)
@@ -148,13 +187,49 @@ while True:
                             pourColor(selectedTube, tube)
                         selectedTube.deselect()
                         selectedTube = None
-                   
+        # MOUSEBUTTONDOWN Event to check player click input
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if input_rect.collidepoint(event.pos):
+                active = True
+            else:
+                active = False
+        # KEYDOWN Event for receiving input
+        elif event.type == pygame.KEYDOWN:
+  
+            # Check for backspace
+            if event.key == pygame.K_BACKSPACE:
+  
+                # get text input from 0 to -1 i.e. end.
+                user_text = user_text[:-1]
+  
+            # Unicode standard is used for string
+            # formation
+            else:
+                user_text += event.unicode       
+    
+    if active:
+        color = color_active
+    else:
+        color = color_passive
+    
+    
+
     #DRAW
     screen.fill("grey")
     for tube in tubes:
         tube.drawColors()
     tubes.draw(screen)   
     screen.blit(text_surface, ((screenWidth/2) - text_surface.get_size()[0]/2 ,100))
+    
+    generatePuzzleButton.update()
+
+    pygame.draw.rect(screen, color, input_rect)
+    text_surface = test_font.render(user_text, True, (0, 0, 0))
+    input_rect.w = max(100, text_surface.get_width()+10)
+    
+    # render at position stated in arguments
+    screen.blit(text_surface, (input_rect.x+5, input_rect.y+5))
+
     if checkWinState(tubes):
         screen.blit(text_win, ((screenWidth/2) - text_surface.get_size()[0]/2 ,200))
 
