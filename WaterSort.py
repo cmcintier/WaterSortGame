27b3ad0e3c2b2ca9f497eb8c148003dc0d3deb98
Chaney McIntier <cmcintier01@gmail.com>
2023-05-30 04:16:07 -0500

    Removed event system, it was a dumb way to do it but cool way to learn about events. Added select functions with working color swap (no visualision)

--- WaterSort.py
+++ WaterSort.py
@@ -10,42 +10,40 @@ class Tube(pygame.sprite.Sprite):
         self.image = pygame.image.load("EmptyTube.png")
         self.rect = self.image.get_rect(topleft = (x,y))
         self.selected = False
-        self.isSearching = False
         self.colors = []
 
     def select(self):
         self.image.set_alpha(100)
         self.selected = True
-        print(isSearching)
 
     def deselect(self):
         self.image.set_alpha(255)
         self.selected = False
     
-    def update(self, events, searchEvents):
+    def setColors(self, colorList):
+        self.colors = colorList
 
-        if self.isSearching:
-            pygame.event.post(pygame.event.Event(SEARCHING_EVENT, color=self.colors))
-
-        for event in events:
-            if event.type == pygame.MOUSEBUTTONUP:
-                if self.rect.collidepoint(event.pos):
-                    if self.selected:
-                        self.deselect()
-                        self.isSearching = False
-                    else:
-                        self.select()
-                        if len(searchEvents) == 1:
-                            print("Two Selected")
-                            self.ifSearching = False
-                        else:
-                            print("One Selected")
-                            self.isSearching = True                   
                     
                     
-    
-    
+def checkCompatible(tube1, tube2):
+    tube1FillHeight = len(tube1.colors)
+    tube2FillHeight = len(tube2.colors)
 
+    if tube1FillHeight == 0:
+        return False
+    elif tube2FillHeight == 0:
+        return True
+    elif tube2FillHeight == 4:
+        return False
+    elif tube1.colors[-1] == tube2.colors[-1]:
+        return True
+    elif tube1.colors[-1] != tube2.colors[-1]:
+        return False
+
+def pourColor(tube1, tube2):
+    tube2.colors.append(tube1.colors.pop(-1))
+    if checkCompatible(tube1, tube2):
+        pourColor(tube1, tube2)
 
 
 
@@ -58,26 +56,57 @@ clock = pygame.time.Clock()
 test_font = pygame.font.Font(None, 50)
 
 tubes = pygame.sprite.Group()
-numTubes = 6
-for i in range(numTubes):
-    tubes.add(Tube( screenWidth / numTubes * i + (screenWidth/4/ numTubes), 300))
-
+# numTubes = 6
+# for i in range(numTubes):
+#     tubes.add(Tube( screenWidth / numTubes * i + (screenWidth/4/ numTubes), 300))
+test_tube1 = Tube(300, 300)
+test_tube1.setColors(["red", "green"])
+test_tube2 = Tube(600, 300)
+test_tube2.setColors(["red", "green"])
+tubes.add(test_tube1)
+tubes.add(test_tube2)
 
 text_surface = test_font.render("Water Sort Puzzle", False, "Black")
 
+selectedTube = None
+
+
 while True:
+
+    
+    
 
     #Checks pygame event system to see if player has quit the game
     isSearching = pygame.event.get(SEARCHING_EVENT)
     events =  pygame.event.get()
     for event in events:
+        # QUIT Event
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        elif event.type == pygame.MOUSEBUTTONUP:
+            print(selectedTube)
+            for tube in tubes:
+                if tube.rect.collidepoint(event.pos):
+                    print(tube.colors)
+                    if tube == selectedTube:
+                        selectedTube = None
+                        tube.deselect()
+                    elif selectedTube == None:
+                        selectedTube = tube
+                        tube.select()
+                        break
+                    else:
+                        if checkCompatible(selectedTube, tube):
+                            pourColor(selectedTube, tube)
+                        selectedTube.deselect()
+                        selectedTube = None
+                   
+                    
+
 
     screen.fill("grey")
 
-    tubes.update(events, isSearching)
     tubes.draw(screen)
     screen.blit(text_surface, ((screenWidth/2) - text_surface.get_size()[0]/2 ,100))
 
